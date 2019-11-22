360b5c54b6cb89416c9f204ff049039f40daf1f2
M13K <ag.koffi@hotmail.fr>
2019-11-22 11:06:42 +0100

    Initialisation d script de deployment de prod

---
+++ deploy-prod.sh
@@ -0,0 +1,25 @@
+#!user/bin/env bash
+
+# Récuperer le code source
+git pull origin master
+
+#Récuperer les librairies
+composer install --dev
+
+# vide le cache
+drush cr
+
+# mettre à jour base donner
+drush updb -y
+
+# Exporte les config de prod
+drush cex -prod -y
+
+
+#Import le config
+drush cim -y
+
+# vide le cache
+drush cr
+
+
